28bf43c1d9529bc06c4513fbed5c8e0271c2247d
Kristijan Kesic <kkesic@student.unipu.hr>
2022-05-21 18:54:52 +0200

    odrađen žzdrijeb

--- src/stranice/rezultat-zdrijeba.tsx
+++ src/stranice/rezultat-zdrijeba.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import {Link} from "react-router-dom";
 
 export const RezultatZdrijeba: React.FC = () => {
+    const Tim1=localStorage.getItem("Tim1");
+
     return (
-        <div> blbaa </div>
+        <div> BLA {Tim1} </div>
     )
 }
--- src/stranice/unos-timova.tsx
+++ src/stranice/unos-timova.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "../style/UnosTimova.css"
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 export const UnosTimova: React.FC = () => {
+    const [Tim1, setTim1]=useState<string>("");
+    const [Tim2, setTim2]=useState<string>("");
+    const [Tim3, setTim3]=useState<string>("");
+    const [Tim4, setTim4]=useState<string>("");
+    const [Tim5, setTim5]=useState<string>("");
+    const [Tim6, setTim6]=useState<string>("");
+    const [Tim7, setTim7]=useState<string>("");
+    const [Tim8, setTim8]=useState<string>("");
+    const navigate = useNavigate();
+
+    const SpremiTimove = () => {
+        const zdrijeb=[Tim1, Tim2, Tim3, Tim4, Tim5, Tim6, Tim7, Tim8];
+        const shuffledzdrijeb = zdrijeb.sort(() => 0.5 - Math.random());
+        const skupinaA=[];
+        const skupinaB=[];
+        for (let i=0; i<zdrijeb.length; i++) {
+            const kuglica=shuffledzdrijeb[i];
+            if (i%2==0) {
+                skupinaA.push(kuglica)
+            } else {
+                skupinaB.push(kuglica)
+            }
+        }
+        localStorage.setItem("skupinaA", JSON.stringify(skupinaA));
+        localStorage.setItem("skupinaB", JSON.stringify(skupinaB));
+        navigate("/rezultat-zdrijeba")
+    }
+
+
+
     return (
         <div className="unos-timova">
             <h2> Unesi osam timova: </h2>
             <div>
-                <input/> <br/>
-                <input/> <br/>
-                <input/> <br/>
-                <input/> <br/>
-                <input/> <br/>
-                <input/> <br/>
-                <input/> <br/>
-                <input/> <br/>
-                <input/> <br/>
+                <input value={Tim1} onChange={(e) => setTim1(e.target.value)}/> <br/>
+                <input value={Tim2} onChange={(e) => setTim2(e.target.value)}/> <br/>
+                <input value={Tim3} onChange={(e) => setTim3(e.target.value)}/> <br/>
+                <input value={Tim4} onChange={(e) => setTim4(e.target.value)}/> <br/>
+                <input value={Tim5} onChange={(e) => setTim5(e.target.value)}/> <br/>
+                <input value={Tim6} onChange={(e) => setTim6(e.target.value)}/> <br/>
+                <input value={Tim7} onChange={(e) => setTim7(e.target.value)}/> <br/>
+                <input value={Tim8} onChange={(e) => setTim8(e.target.value)}/> <br/>
             </div>
             <div>
-                <Link to="/rezultat-zdrijeba">
-                    <button className="button-zdrijeb"> ŽDRIJEB </button>
-                </Link>
+                    <button onClick={SpremiTimove} className="button-zdrijeb"> ŽDRIJEB </button>
             </div>
         </div>
     )
